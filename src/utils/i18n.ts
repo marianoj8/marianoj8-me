b2dea241d29d83d4332b8884c9b2e178cafdbba2
Mariano JavaSwing <marianogoncalves8@gmail.com>
2025-06-08 15:11:05 +0100

    Refactor i18n resources to utilize external localization files for better maintainability

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -8,30 +8,8 @@ import pt from '../locales/pt.json';
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
 const resources = {
-    en: {
-        translation: {
-            aboutMe: "About Me",
-            technicalSkills01: "Technical Skills",
-            technicalSkills02: "Technical Skills",
-            microservices: "Microservices",
-            highlightKeyProjects: "Highlight key projects",
-            projects: "Projects",
-            contact: "Contact",
-            aboutMeContent: "Senior Developer with over 11 years of experience in technologies such as Java, Spring, Kafka, MySQL, PostgreSQL, and Dart. Expert in microservices architecture and scalable system development. Passionate about creating innovative solutions, I have worked on challenging projects, ranging from high-performance systems to modern mobile applications and online payment system integrations."
-        }
-    },
-    pt: {
-        translation: {
-            aboutMe: "Sobre Mim",
-            technicalSkills01: "Habilidades",
-            technicalSkills02: "Habilidades Técnicas",
-            microservices: "Microserviços",
-            highlightKeyProjects: "Projetos Destacados",
-            projects: "Projetos",
-            contact: "Contato",
-            aboutMeContent: "Desenvolvedor sênior com mais de 11 anos de experiência em tecnologias como Java, Spring, Kafka, MySQL, PostgreSQL e Dart. Especialista em arquitetura de microserviços e desenvolvimento de sistemas escaláveis. Apaixonado por criar soluções inovadoras, já trabalhei em projetos desafiadores, desde sistemas de alta performance até aplicativos móveis modernos e integração de sistemas de pagamentos online.",
-        }
-    }
+    en: { translation: en },
+    pt: { translation: pt },
 };
 
 i18n
