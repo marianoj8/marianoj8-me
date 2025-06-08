f0418ce834fff73b35e2b5b05032ed6af6e07087
Mariano JavaSwing <marianogoncalves8@gmail.com>
2025-06-08 15:10:57 +0100

    Refactor i18n module to import localization files for English and Portuguese

--- src/utils/i18n.ts
+++ src/utils/i18n.ts
@@ -1,6 +1,9 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 
+import en from '../locales/en.json';
+import pt from '../locales/pt.json';
+
 // the translations
 // (tip move them in a JSON file and import them,
 // or even better, manage them separated from your code: https://react.i18next.com/guides/multiple-translation-files)
